645da0dd293286bfdcd2e4ad77b9fabc9285225a
aishita04 <98046402+aishita04@users.noreply.github.com>
2022-06-12 17:56:24 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sum {
+   public static void main(String args[]) {
+      int num = 0;
+      for (int i = 0; i < args.length; i++) {
+         num += Integer.parseInt(args[i]);
+      }
+
+      System.out.println("The sum is " + num);
+   }
+}
